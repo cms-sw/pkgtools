c2eb17bcbcbdfe7fc6725ea17cfc5238828fe6e5
Andrew Melo <andrew.melo@gmail.com>
2015-04-30 16:25:11 -0500

    Fix typo for OSX building
    * Correct "quess" to "guess"
    * Detection of $CONFIG_BUILD should happen after $ARCH is set

--- build_rpm.sh
+++ build_rpm.sh
@@ -4,16 +4,6 @@
 
 PREFIX=/usr/local/bin
 BUILDPROCESSES=2
-case ${ARCH} in
-  osx*)
-    # Darwin is not RPM based, explicitly go for quessing the triplet
-    CONFIG_BUILD=quess
-    ;;
-  *)
-    # Assume Linux distro is RPM based and fetch triplet from RPM
-    CONFIG_BUILD=auto
-    ;;
-esac
 
 while [ $# -gt 0 ]
 do
@@ -64,6 +54,17 @@ do
     ;;
   esac
 done
+
+case ${ARCH} in
+  osx*)
+    # Darwin is not RPM based, explicitly go for guessing the triplet
+    CONFIG_BUILD=guess
+    ;;
+  *)
+    # Assume Linux distro is RPM based and fetch triplet from RPM
+    CONFIG_BUILD=auto
+    ;;
+esac
 
 set -e
 
