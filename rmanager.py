b27ca37c8a6521541a8670a8279d169ccd97626c
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-03-18 10:08:01 +0100

    sync to v00-33-xx

--- rmanager.py
+++ rmanager.py
@@ -1,47 +1,54 @@
+import re
 class ResourceManager(object):
-    def __init__(self, ESstats, cpu_percent_usage, memory_percent_usage, njobs):
-        self.machineResources = { "total": {"cpu_90": cpu_percent_usage*int(ESstats["MachineCPUCount"]),
-                                            "rss_75" : memory_percent_usage*int(ESstats["MachineMemoryGB"])*10737418 },
-                                  "available": {"cpu_90": cpu_percent_usage*int(ESstats["MachineCPUCount"]),
-                                            "rss_75" : memory_percent_usage*int(ESstats["MachineMemoryGB"])*10737418 } }
-        self.externalsStats = ESstats["externals"]# this should be get before running pkgtools
-        self.resourcesAllocatedForExternals = {} # resources that were alocated for given externals, say root -> "root": {"rss":"", cpu:""}
-        self.priorityList = ["time", "cpu_90"] # can be any list from the stat keys
-        self.missingExternalsStrategy = None # runFirst, runLast
+    def __init__(self, ESstats, scheduler, highestPriortyOnly = False):
+        self.esStats = ESstats
+        self.scheduler = scheduler
+        self.machineResources = ESstats["resources"]
+        self.resouceList = ["cpu", "rss"]
+        self.allocated = {}
+        self.highestPriortyOnly = highestPriortyOnly
+        self.priorityList = ["time"] # can be any list from the stat keys
     
-    def allocResourcesForExternals(self, externalsList=[]): # return ordered list for externals that can be started
-        # first, strip the names from build-*++version and make a tmp name to full name dict.
-        # toolfiles should not arrive here, but will also work if they do
-        ext_name_to_fullname_dict = {}
-        for e in externalsList:
-            short_name = e.split('+')[1]
-            ext_name_to_fullname_dict[short_name]=e
-        externalsList_shortNames = ext_name_to_fullname_dict.keys()
-        # If record for an external is not available, allocate it half the cpu and 1/6th RAM from the resources to run it
-        # OR allocate all resources for each missing external, forcing them to build last one by one.
+    def allocResourcesForExternals(self, externalsList, count=1000): # return ordered list for externals that can be started
         externals_to_run = []
-        for ext in externalsList_shortNames:
-            if ext not in self.externalsStats:
-                # external is not found, this should happen only for new externals. get the first element whichever it is and change its properties
-                print 'external: ', ext, 'not found, create entry for it'
-                self.externalsStats[ext] = [{"name":ext, "cpu_90":self.machineResources["total"]["cpu_90"]/2,"rss_75":self.machineResources["total"]["rss_75"]/6,"time":1800 }]
-            externals_to_run.append(self.externalsStats[ext][0])
+        if count<=0: return externals_to_run
+        for ext_full in externalsList:
+            stats = {"name": ext_full}
+            ext = ext_full.split('+')[1]
+            if ext not in self.esStats["packages"]:
+                idx = -1
+                for exp in self.esStats["known"]:
+                    if re.match(exp[0], ext):
+                        idx = exp[1]
+                        break
+                for k in self.esStats["defaults"]:
+                    stats[k] = self.esStats["defaults"][k][idx]
+                self.scheduler.log("New external found, creating default entry %s" % stats)
+            else:
+                for k in self.esStats["defaults"]:
+                    stats[k] = self.esStats["packages"][ext][k]
+            externals_to_run.append(stats)
         # first order them by metric and then run over to alloc resources
         externalsList_sorted = [ext for ext in sorted(externals_to_run, key=lambda x: tuple(x[k] for k in self.priorityList), reverse=True)]
-
-        externals_to_run = []
+        externals_ordered = []
         for ex_stats in externalsList_sorted:
-            if (ex_stats["cpu_90"]<=self.machineResources["available"]["cpu_90"] and ex_stats["rss_75"]<=self.machineResources["available"]["rss_75"]):
-                self.resourcesAllocatedForExternals[ex_stats["name"]] = {}
-                for prm in ["rss_75", "cpu_90"]:
-                    self.machineResources["available"][prm] -= ex_stats[prm]
-                    self.resourcesAllocatedForExternals[ex_stats["name"]][prm] = ex_stats[prm]
-                externals_to_run.append(ext_name_to_fullname_dict[ex_stats["name"]]) # this gets the full names
-        return externals_to_run
+            if not [r for r in self.resouceList if ex_stats[r]>self.machineResources[r]]:
+                for prm in self.resouceList:
+                    self.machineResources[prm] -= ex_stats[prm]
+                externals_ordered.append(ex_stats["name"])
+                self.allocated[ex_stats["name"]] = ex_stats
+                self.scheduler.log("Allocating resouces %s" % ex_stats)
+                count-=1
+                if count<=0:
+                  break
+            elif self.highestPriortyOnly:
+              break
+        self.scheduler.log("Available resouces %s" % self.machineResources)
+        return externals_ordered
 
-    def releaseResourcesForExternal(self, external): # external name
-        # get the external name only
-        external = external.split('+')[1]
-        for prm in ["rss_75", "cpu_90"]:
-            self.machineResources["available"][prm] += self.resourcesAllocatedForExternals[external][prm]
-        del self.resourcesAllocatedForExternals[external]
+    def releaseResourcesForExternal(self, external):
+        if external not in self.allocated: return
+        for prm in self.resouceList:
+            self.machineResources[prm] +=  self.allocated[external][prm]
+        self.scheduler.log("Released resources: %s , %s" % (self.allocated[external], self.machineResources))
+        del self.allocated[external]
