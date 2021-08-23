7a8ad3ada365bf5191fd3e27e88e295aef89a590
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-08-23 13:12:12 +0200

    cleanup NO_AUTO_RUNPATH

--- cmsdist_config.py
+++ cmsdist_config.py
@@ -1,3 +1,3 @@
 USE_COMPILER_VERSION = [ "slc7_aarch64_gcc820", "slc7_ppc64le_gcc820" ]
 NO_VERSION_SUFFIX = [ "cmssw", "cmssw-patch", "cmssw-ib"]
-NO_AUTO_RUNPATH = [ "cmssw", "cmssw-patch", "cmssw-ib"]
+NO_AUTO_RUNPATH = []
