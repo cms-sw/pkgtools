f2fe6b8e9fca95dcc998440a5272d25129ae27f4
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-10-25 13:51:18 +0200

    no prefix for fwlite

--- cmsdist_config.py
+++ cmsdist_config.py
@@ -1,3 +1,3 @@
 USE_COMPILER_VERSION = [ "slc7_aarch64_gcc820", "slc7_ppc64le_gcc820" ]
-NO_VERSION_SUFFIX = [ "cmssw", "cmssw-patch", "cmssw-ib"]
+NO_VERSION_SUFFIX = [ "cmssw", "cmssw-patch", "cmssw-ib", "fwlite", "fwlite-patch"]
 NO_AUTO_RUNPATH = []
