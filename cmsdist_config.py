1f9e62ad216152d047461456a14487863d05dd88
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2021-11-24 09:02:10 +0100

    no version suffix for data packages

--- cmsdist_config.py
+++ cmsdist_config.py
@@ -1,3 +1,8 @@
+from re import compile
 USE_COMPILER_VERSION = [ "slc7_aarch64_gcc820", "slc7_ppc64le_gcc820" ]
-NO_VERSION_SUFFIX = [ "cmssw", "cmssw-patch", "cmssw-ib", "fwlite", "fwlite-patch"]
+NO_VERSION_SUFFIX = [
+  compile("cmssw"), compile("cmssw-patch"), compile("cmssw-ib"),
+  compile("fwlite"), compile("fwlite-patch"),
+  compile("^data-[A-Z][A-Za-z0-9]+-[A-Za-z][A-Za-z0-9]+$")
+]
 NO_AUTO_RUNPATH = []
