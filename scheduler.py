9848c5f7fa2aa683eb60ec286ad8d69c29dca874
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2024-07-15 17:36:49 +0200

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -88,10 +88,10 @@ class Scheduler(object):
         dump_cnt += wait_gap
         if dump_cnt >= dump_status:
           dump_cnt = 0
-          self.log("Worker Queue:  %s" % self.workersQueue.qsize())
-          self.log("Result Queue:  %s" % self.resultsQueue.qsize())
-          self.log("Running tasks: %s" % self.runningJobs)
-          self.log("Pending tasks: %s" % self.pendingJobs)
+          print("Worker Queue:  %s" % self.workersQueue.qsize())
+          print("Result Queue:  %s" % self.resultsQueue.qsize())
+          print("Running tasks: %s" % self.runningJobs)
+          print("Pending tasks: %s" % self.pendingJobs)
       except KeyboardInterrupt:
         print ("Ctrl-c received, waiting for workers to finish")
         while self.workersQueue.full():
