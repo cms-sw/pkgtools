153ec302a2c9804d8248b75b10b4bc35dd46b38f
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-11-19 11:39:19 +0100

    allow to have separate stats for prep/build/install/packaging

--- rmanager.py
+++ rmanager.py
@@ -15,7 +15,10 @@ class ResourceManager(object):
         for ext_full in externalsList:
             stats = {"name": ext_full}
             ext = ext_full.split('+')[1]
-            if ext not in self.esStats["packages"]:
+            ext_items = ext_full.split("-")
+            build_type = ext_items[1] if ext_items[1] in ["prep", "build", "install", "srpm", "rpms"] else ""
+            pkg_stats = self.esStats[build_type]["packages"] if build_type else self.esStats["packages"]
+            if ext not in pkg_stats:
                 idx = -1
                 for exp in self.esStats["known"]:
                     if re.match(exp[0], ext):
@@ -26,7 +29,7 @@ class ResourceManager(object):
                 self.scheduler.log("New external found, creating default entry %s" % stats)
             else:
                 for k in self.esStats["defaults"]:
-                    stats[k] = self.esStats["packages"][ext][k]
+                    stats[k] = pkg_stats[ext][k]
             externals_to_run.append(stats)
         # first order them by metric and then run over to alloc resources
         externalsList_sorted = [ext for ext in sorted(externals_to_run, key=lambda x: tuple(x[k] for k in self.priorityList), reverse=True)]
