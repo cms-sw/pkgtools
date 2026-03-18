86da3b72e2b3154f5362112c3908ddc7a9da97bc
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2026-03-18 12:27:59 +0100

    use cms specific ninja nv variable instead of change official ninja

--- build-system-env.sh
+++ build-system-env.sh
@@ -1,10 +1,14 @@
 export CMAKE_BUILD_PARALLEL_LEVEL="$1"
 export MAX_JOBS="$1"
-export NINJA_NUM_JOBS="$1"
+export CMS_NINJA_NUM_JOBS="$1"
 export CYTHON_NTHREADS="$1"
 export MESON_NUM_PROCESSES="$1"
+
+#Env variables OMP_* are to limit the rpmdeps (which is run during rpmbuild packaging stage)
 export OMP_NUM_THREADS="$1"
 export OMP_THREAD_LIMIT="$1"
 export OMP_DYNAMIC=FALSE
+
+# To limit the parallel process run by cargo
 export CARGO_BUILD_JOBS="$1"
 export CARGO_HOME="${TMPDIR}/cargo_home"
